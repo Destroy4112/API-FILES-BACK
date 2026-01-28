5e71f91bfdab4aa92bb94460cbb15904ee4d2fff
Destroy4112 <juanfearrietaortiz@gmail.com>
2026-01-28 14:27:04 -0500

    Correccion entity

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { File } from './entities/file.entity';
 import { FilesController } from './files.controller';
 import { FilesService } from './files.service';
 import { S3Service } from './s3.service';
